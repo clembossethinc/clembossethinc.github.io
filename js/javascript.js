f66239018ce298357f5c2518e6d84f22da28c805
bosseth123 <46276579+bosseth123@users.noreply.github.com>
2019-01-04 14:20:34 +0100

    update1

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+
+
+<script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+
+
+
+ScrollReveal().reveal('h2', {delay:100, usedelay:"onload"});
+ScrollReveal().reveal('h3', {delay:100, usedelay:"onload"});
+
+ScrollReveal().reveal('h1', {delay:100, usedelay:"onload"});
+ScrollReveal().reveal('.button', {delay:100, usedelay:"onload"});
+ScrollReveal().reveal('p', {delay:100, usedelay:"onload"});
+ScrollReveal().reveal('img', {delay:100, usedelay:"onload"});
+ScrollReveal().reveal('form', {delay:100, usedelay:"onload"});
+ScrollReveal().reveal('header img', {delay:0, usedelay:"onload"});
